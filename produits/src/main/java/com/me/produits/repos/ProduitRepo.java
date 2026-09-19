0f963c83e406cdbb42b326d1b91af3d6864d776e
awani-dev <awanix@proton.me>
2026-09-19 13:44:50 +0100

    Delete produits directory

--- produits/src/main/java/com/me/produits/repos/ProduitRepo.java
+++
@@ -1,9 +0,0 @@
-package com.me.produits.repos;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.me.produits.entities.Produit;
-
-public interface ProduitRepo extends JpaRepository<Produit, Long> {
-
-}
